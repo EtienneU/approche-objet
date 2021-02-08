6ee741c9d9c5f29433dffa7ba3eb365b8745643e
EtienneUrbano <urbano.etienne@laposte.net>
2021-02-08 10:39:23 +0100

    Ajout TP 1 du J9 (StringBuilder)

---
+++ src/fr/diginamic/chaines/TestStringBuilder.java
@@ -0,0 +1,22 @@
+package fr.diginamic.chaines;
+
+public class TestStringBuilder {
+
+	public static void main(String[] args) {
+		
+		String s = "";
+		StringBuilder builder = new StringBuilder();
+		
+		long debut = System.currentTimeMillis();
+		
+		for (int i = 0; i < 100_000; i++) {
+//			s += i;
+			builder.append(i);
+		}
+		
+		long fin = System.currentTimeMillis(); 
+		
+		System.out.println("Temp d'exécution : " + (fin - debut) + " ms");
+	}
+
+}
