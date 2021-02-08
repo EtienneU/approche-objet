22aa194db9be6b1f7961d39a659f2518a313c722
EtienneUrbano <urbano.etienne@laposte.net>
2021-02-08 16:48:33 +0100

    MAJ TP .equals()

---
+++ src/fr/diginamic/listes/essais/TestEqualsVille.java
@@ -0,0 +1,19 @@
+package fr.diginamic.listes.essais;
+
+import fr.diginamic.listes.Ville;
+
+public class TestEqualsVille {
+
+	public static void main(String[] args) {
+
+		Ville v1 = new Ville("Winterfell", 568);
+		Ville v2 = new Ville("Winterfell", 568);
+		Ville v3 = v2;
+		System.out.println("v1.equals(v2) vaut " + v1.equals(v2));
+		System.out.println("v1 == v2 vaut " + (v1 == v2)); 
+		// false car v1 et v2 pointent vers des emplacements mémoire différents		
+		System.out.println("v2 == v3 vaut " + (v2 == v3));
+
+	}
+
+}
