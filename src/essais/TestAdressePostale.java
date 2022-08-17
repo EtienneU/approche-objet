1e1aaef055038e9d7abc70717749b7d9d67f0da8
Etienne URBANO_MeritisRegions <etienne.urbano@meritis.fr>
2022-08-17 11:44:56 +0200

    test connexion Meritis - objets adresses

--- src/essais/TestAdressePostale.java
+++ src/essais/TestAdressePostale.java
@@ -4,16 +4,14 @@ import entites.AdressePostale;
 
 public class TestAdressePostale {
 	
-	
 	public static void main (String[] args) {
 		
 		AdressePostale a1 = new AdressePostale(1044, "Chemin du Paradis", "69004", "Lyon");
-		
 		AdressePostale a2 = new AdressePostale(355, "Rue des Teinturiers", "84000", "Avignon");
 		
 		System.out.println(a1); 	// appel de la méthode toString()
 		a1.afficheAdressePostale(); // appel de ma méthode afficheAdressePostale()
 		
-	}
-	
+		System.out.println(a2);
+	}	
 }
