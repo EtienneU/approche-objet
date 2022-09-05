d2887ed834a40e4b2f0ef4ae8622560f7ff3e968
EtienneUrbano <urbano.etienne@laposte.net>
2022-09-05 18:23:22 +0200

    Listes et expression lambda

--- src/fr/diginamic/lambda/testLambda.java
+++ src/fr/diginamic/lambda/testLambda.java
@@ -20,17 +20,17 @@ public class testLambda {
 	public static void main(String[] args) {
 		
 		List<Personne> personnes = new ArrayList<>();
-		personnes.add(new Personne("Duchêne", "Claude", 39));
+		personnes.add(new Personne("Duchene", "Claude", 39));
 		personnes.add(new Personne("Dujardin", "Melanie", 76));
 		personnes.add(new Personne("Krank", "Giselle", 26));
 		personnes.add(new Personne("Borne", "Jonathan", 15));
+		personnes.add(new Personne("Toraidu", "Chloe", 17));
 
 		Predicate predicateAge = p -> p.getAge() >= AGE_MAJORITE;
-		Predicate predicateNom = p -> p.getNom().contains("Du");
+		Predicate predicateNom = p -> p.getNom().toLowerCase().contains("du");
 		
-		System.out.println(personnes.get(3).getPrenom() + " est une personne majeure : " + predicateAge.filter(personnes.get(3)));
-		System.out.println(personnes.get(1).getPrenom() + " " + personnes.get(1).getNom() + " contient le terme 'Du' : " + predicateNom.filter(personnes.get(1)));
-
+		System.out.println(personnes.get(0).getPrenom() + " est une personne majeure ? : " + predicateAge.filter(personnes.get(0)));
+		System.out.println(personnes.get(personnes.size() - 1).getPrenom() + " est une personne majeure ? : " + predicateAge.filter(personnes.get(personnes.size() - 1)));
+		System.out.println(personnes.get(1).getPrenom() + " " + personnes.get(1).getNom() + " contient le terme 'Du' ? : " + predicateNom.filter(personnes.get(1)));
 	}
-
 }
