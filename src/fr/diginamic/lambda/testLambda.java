29697e479d7f8b3916feb29771100aefc74e4b5d
EtienneUrbano <urbano.etienne@laposte.net>
2022-09-20 11:45:42 +0200

    MAJ Lambda et reference de methode

--- src/fr/diginamic/lambda/testLambda.java
+++ src/fr/diginamic/lambda/testLambda.java
@@ -5,6 +5,7 @@ package fr.diginamic.lambda;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * @author EtienneUrbano
@@ -39,8 +40,36 @@ public class testLambda {
 			+ 	personnes.get(4).getPrenom() + " " + personnes.get(4).getNom() + " contient le terme 'Du' ? : " + affirmerNom.filter(personnes.get(4))
 		);
 
+
+		// ----- un autre exemple avec la même liste de personnes -----
+		// Je transforme une collection de Personne en collection de String representant leur nom
+		// ou en collection d'entiers représentant leur age multiplie par 2
+		Function<Personne, String> f1 = p -> p.getNom();
+		// ou utiliser la reference de methode :   Function<Personne, String> f1 = Personne::getNom;
+		Function<Personne, Integer> f2 = (Personne p) -> p.getAge() * 2;
+		System.out.println(transformToListString(personnes, f1));
+		System.out.println(transformToListInt(personnes, f2));
+
+
 		// ----- un autre exemple -----
 		Etudiant etu = leNomAafficher -> System.out.println("Je me nomme " + leNomAafficher);
 		etu.donnerNom("Marie-Claude");
+	}
+
+	public static List<String> transformToListString(List<Personne> list, Function<Personne, String> func){
+		List<String> ls = new ArrayList<>();
+		for (Personne p : list) {
+			ls.add(func.apply(p));
+			//func.apply(p) retournera ici le nom de l'objet Personne
+		}
+		return ls;
+	}
+	public static List<Integer> transformToListInt(List<Personne> list, Function<Personne, Integer> func){
+		List<Integer> ls = new ArrayList<>();
+		for (Personne p : list) {
+			ls.add(func.apply(p));
+			//func.apply(p) retournera ici l'âge multiplié par 2 de l'objet Personne
+		}
+		return ls;
 	}
 }
